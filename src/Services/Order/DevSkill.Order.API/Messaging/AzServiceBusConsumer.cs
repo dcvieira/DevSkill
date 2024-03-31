c249d752933cde1ff4d4884cbb862440ae4d160f
DIEGO CAMILO MARTINS VIEIRA <dcvieira@tjsp.jus.br>
2024-03-30 22:24:54 -0300

    ajustes de services registration

--- src/Services/Order/DevSkill.Order.API/Messaging/AzServiceBusConsumer.cs
+++ src/Services/Order/DevSkill.Order.API/Messaging/AzServiceBusConsumer.cs
@@ -1,7 +1,8 @@
-﻿using DevSkill.Integration.Messages;
-using DevSkill.Order.Application.Features.Order.Commands.CreateOrder;
+﻿
+using DevSkill.Integration.Messages;
+using DevSkill.Order.Application.Contracts.Persistence;
 using DevSkill.Order.Application.Messages;
-using MediatR;
+using DevSkill.Order.Domain.ValueObjects;
 using Microsoft.Azure.ServiceBus;
 using Microsoft.Azure.ServiceBus.Core;
 using Newtonsoft.Json;
@@ -12,31 +13,23 @@ public class AzServiceBusConsumer : IAzServiceBusConsumer
 {
     private readonly string subscriptionName = "devskillorder";
     private readonly IReceiverClient checkoutMessageReceiverClient;
-    private readonly IReceiverClient orderPaymentUpdateMessageReceiverClient;
 
     private readonly IConfiguration _configuration;
 
-    private readonly IMediator _mediator;
-    private readonly IMessageBus _messageBus;
+    private readonly IServiceProvider _serviceProvider;
 
     private readonly string checkoutMessageTopic;
-    private readonly string orderPaymentRequestMessageTopic;
-    private readonly string orderPaymentUpdatedMessageTopic;
 
-    public AzServiceBusConsumer(IConfiguration configuration, IMessageBus messageBus, IMediator mediator)
+    public AzServiceBusConsumer(IConfiguration configuration, IMessageBus messageBus, IServiceProvider serviceProvider)
     {
         _configuration = configuration;
 
-        _mediator = mediator;
-        _messageBus = messageBus;
+        _serviceProvider = serviceProvider;
 
         var serviceBusConnectionString = _configuration.GetValue<string>("ServiceBusConnectionString");
         checkoutMessageTopic = _configuration.GetValue<string>("CheckoutMessageTopic");
-        orderPaymentRequestMessageTopic = _configuration.GetValue<string>("OrderPaymentRequestMessageTopic");
-        orderPaymentUpdatedMessageTopic = _configuration.GetValue<string>("OrderPaymentUpdatedMessageTopic");
 
         checkoutMessageReceiverClient = new SubscriptionClient(serviceBusConnectionString, checkoutMessageTopic, subscriptionName);
-        orderPaymentUpdateMessageReceiverClient = new SubscriptionClient(serviceBusConnectionString, orderPaymentUpdatedMessageTopic, subscriptionName);
     }
 
     public void Start()
@@ -54,33 +47,35 @@ public class AzServiceBusConsumer : IAzServiceBusConsumer
         //save order with status not paid
         CheckoutRequestMessage checkoutMessage = JsonConvert.DeserializeObject<CheckoutRequestMessage>(body);
 
-
-        var command = new CreateOrderCommand
+        using (var scope = _serviceProvider.CreateScope())
         {
-            UserId = checkoutMessage.UserId,
-            CoursePrice = checkoutMessage.CoursePrice,
-            CourseId = checkoutMessage.CourseId,
-            CourseName = checkoutMessage.CourseName,
-            Country = checkoutMessage.Country,
-            PostalCode = checkoutMessage.PostalCode,
-            NameOnCard = checkoutMessage.NameOnCard,
-            CreditCardNumber = checkoutMessage.CreditCardNumber,
-            SecuityCode = checkoutMessage.SecuityCode,
-            ExpirationDate = checkoutMessage.ExpirationDate
-        };
-        
-        await _mediator.Send(command);
 
+            var orderRepository = scope.ServiceProvider.GetRequiredService<IOrderRepository>();
+
+
+            var Order = new Domain.Entities.Order()
+            {
+                CourseId = checkoutMessage.CourseId,
+                Price = checkoutMessage.CoursePrice,
+                CourseName = checkoutMessage.CourseName,
+                UserId = checkoutMessage.UserId,
+                OrderDate = DateTime.Now,
+                OrderStatus = Domain.Enums.OrderStatus.Completed,
+                OrderDetails = new Domain.Entities.OrderDetails
+                {
+                    Country = checkoutMessage.Country,
+                    PostalCode = checkoutMessage.PostalCode,
+                    NameOnCard = checkoutMessage.NameOnCard,
+                    CreditCardNumber = CreditCardNumber.From(checkoutMessage.CreditCardNumber),
+                    SecuityCode = checkoutMessage.SecuityCode,
+                    ExpirationDate = checkoutMessage.ExpirationDate
+                }
+
+            };
+            await orderRepository.AddAsync(Order);
+
+        }
     }
-
-    //private async Task OnOrderPaymentUpdateReceived(Message message, CancellationToken arg2)
-    //{
-    //    var body = Encoding.UTF8.GetString(message.Body);//json from service bus
-    //    OrderPaymentUpdateMessage orderPaymentUpdateMessage =
-    //        JsonConvert.DeserializeObject<OrderPaymentUpdateMessage>(body);
-
-    //    await _orderRepository.UpdateOrderPaymentStatus(orderPaymentUpdateMessage.OrderId, orderPaymentUpdateMessage.PaymentSuccess);
-    //}
 
     private Task OnServiceBusException(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
     {
